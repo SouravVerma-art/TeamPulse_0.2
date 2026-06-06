9371b11e81e7c72b1b250907ba958ee0d6027a97
SouravVerma-art <souravverma769@gmail.com>
2026-06-06 06:11:01 -0700

    Update next.config.js

--- teampulse-dashboard/next.config.js
+++ teampulse-dashboard/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   // We can add rewrites if we want to proxy the backend,
   // but the code uses direct URLs for SSE.
 };
